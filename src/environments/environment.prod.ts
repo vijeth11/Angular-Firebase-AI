3372ba841670b436eb5a2f3ff9b0ec19cac1d286
Vijeth Belle <vijeth11@yahoo.co.in>
2025-06-13 13:28:11 +0530

    added product cards and images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,12 @@
 export const environment = {
   production: false,
   firebase: {
+    apiKey: "AIzaSyDCEu3Ce2aI5DQcAyfp_quQk95HBxN2RXk",
+    authDomain: "angular-grocery-assistance.firebaseapp.com",
+    projectId: "angular-grocery-assistance",
+    storageBucket: "angular-grocery-assistance.firebasestorage.app",
+    messagingSenderId: "592291241286",
+    appId: "1:592291241286:web:e80968d2da6c34913c1947",
+    measurementId: "G-MM7MYCYF0K"
   },
 };
